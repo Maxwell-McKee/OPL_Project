801d8cc7bc0dc9d205a98ec25d7fc7c84671d400
max mckee <max mckee>
2017-04-12 15:22:15 -0700

    More experimenting

--- Demo GUI/Demo GUI/Program.fs
+++ Demo GUI/Demo GUI/Program.fs
@@ -7,11 +7,19 @@ form.Visible <- true
 form.Text <- "Test Form"
 form.Size <- new Size(1000, 1000)
 
+let box = new ComboBox()
+let comboItems = new ComboBox.ObjectCollection(box)
+comboItems.Add("One") |> ignore
+comboItems.Add("Two") |> ignore
+comboItems.Add("Three") |> ignore
+
+
 let label = new Label()
 label.Text <- "Hello World"
 label.Location <- new Point(25, 25)
 
 form.Controls.Add(label)
+form.Controls.Add(box)
 
 [<STAThread>]
 Application.Run(form)
