6f097b74b9f490cce7719a352fe04023cb89b94c
max mckee <max mckee>
2017-04-12 11:10:40 -0700

    GUI experimentation with .NET framework

---
+++ Demo GUI/Demo GUI/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open System.Drawing
+open System.Windows.Forms
+
+let form = new Form()
+form.Visible <- true
+form.Text <- "Test Form"
+form.Size <- new Size(1000, 1000)
+
+let label = new Label()
+label.Text <- "Hello World"
+label.Location <- new Point(25, 25)
+
+form.Controls.Add(label)
+
+[<STAThread>]
+Application.Run(form)
