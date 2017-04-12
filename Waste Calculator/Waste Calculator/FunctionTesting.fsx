65bf58f89a5fe78cc6247d53f1aa1e6d3c0ce890
max mckee <max mckee>
2017-04-11 23:22:52 -0700

    added functions for calculator

--- Waste Calculator/Waste Calculator/FunctionTesting.fsx
+++ Waste Calculator/Waste Calculator/FunctionTesting.fsx
@@ -1,4 +1,10 @@
-﻿let myTimesTwo x = x + x
+﻿let napToTree napkins = napkins / 900.0
+let paperToReams paper = paper / 500.0
+let reamsToPaper reams = reams * 500.0
+let paperToTrees paper = (paper / 500.0) * 0.06
 
-let myTimesFour x =
-    myTimesTwo x |> myTimesTwo
+let treeToNap trees = trees * 900.0
+let treesToPaper trees = (trees / 0.06) * 500.0
+
+let paperToPaper func1 measure1 func2 = 
+  func1 measure1 |> func2
